c78baa5a543bac69196521ddfe448b82254a7311
Kurt Petrek <kurtpetrek@gmail.com>
2017-05-06 16:58:31 -0400

    render multiple apts using map on data within component

--- process/js/app.js
+++ process/js/app.js
@@ -2,17 +2,67 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var MainInterface = React.createClass({
+  getInitialState: function(){
+    return {
+      data: [
+        {
+          "petName": "Buffy",
+          "ownerName": "Hassum Harrod",
+          "aptDate": "2016-06-20 15:30",
+          "aptNotes": "This Chihuahua has not eaten for three days and is lethargic"
+        },
+        {
+          "petName": "Spot",
+          "ownerName": "Constance Smith",
+          "aptDate": "2016-06-24 08:30",
+          "aptNotes": "This German Shepherd is having some back pain"
+        },
+        {
+          "petName": "Goldie",
+          "ownerName": "Barot Bellingham",
+          "aptDate": "2016-06-22 15:50",
+          "aptNotes": "This Goldfish has some weird spots in the belly"
+        },
+        {
+          "petName": "Mitten",
+          "ownerName": "Hillary Goldwyn",
+          "aptDate": "2016-06-21 9:15",
+          "aptNotes": "Cat has excessive hairballs"
+        }
+      ]
+    } // Return
+  }, // getInitialState
+
   render: function() {
+    var filteredApts = this.state.data;
+
+    filteredApts = filteredApts.map(function(item, index){
+      return (
+          <li className="pet-item media" key={index}>          
+            <div className="pet-info media-body">
+              <div className="pet-head">
+                <span className="pet-name">{ this.state.data[index].petName }</span>
+                <span className="apt-data pull-right">{ this.state.data[index].aptDate} </span>
+              </div>
+              <div className="owner-name">
+                <span className="label-item">Owner:</span>
+                {this.state.data[index].ownerName}
+              </div>
+              <div className="apt-notes">
+                {this.state.data[index].aptNotes}
+              </div>
+            </div>
+          </li>
+      ) // return
+    }.bind(this)); // filteredApts.map
+
     return (
       <div className="interface">
-        <h1>Pet Appointments</h1>
-        <ul>
-          <li>Buffy 3:30PM</li>
-          <li>Spot 8:30PM</li>
-          <li>Goldie 3:50PM</li>
+        <ul className="item-list media-list">
+          {filteredApts}
         </ul>
       </div>
-    )
+    ) //return
   } //render
 }); //MainInterface
 

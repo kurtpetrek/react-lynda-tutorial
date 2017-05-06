ab88af9f6a282465fa999385974dfb327fa83c44
Kurt Petrek <kurtpetrek@gmail.com>
2017-05-06 19:49:40 -0400

    added search functionality , finished tutorial and updated readme

--- process/js/app.js
+++ process/js/app.js
@@ -4,11 +4,15 @@ var _ = require('lodash');
 
 var AptList = require('./AptList');
 var AddAppointment = require('./AddAppointment');
+var SearchAppointments = require('./SearchAppointments');
 
 var MainInterface = React.createClass({
   getInitialState: function(){
     return {
       aptBodyVisible: false,
+      orderBy: 'petName',
+      orderDir: 'asc',
+      queryText: '',
       myAppointments: []
     } // Return
   }, // getInitialState
@@ -47,10 +51,43 @@ var MainInterface = React.createClass({
     this.setState({
       myAppointments: tempApts
     });//setState
+    this.toggleAddDisplay();
   },//addItem
 
+  reOrder: function(orderBy, orderDir) {
+    this.setState({
+      orderBy: orderBy,
+      orderDir: orderDir
+    });
+  },
+
+  searchApts: function(q){
+    this.setState({
+      queryText: q
+    });
+  },
+
   render: function() {
-    var filteredApts = this.state.myAppointments;
+    var filteredApts = [];
+    var orderBy = this.state.orderBy;
+    var orderDir = this.state.orderDir;
+    var queryText = this. state.queryText;
+    var myAppointments = this.state.myAppointments;
+
+    myAppointments.forEach(function(item){
+      if(
+        (item.petName.toLowerCase().indexOf(queryText) != -1) ||
+        (item.ownerName.toLowerCase().indexOf(queryText) != -1) ||
+        (item.aptDate.toLowerCase().indexOf(queryText) != -1) ||
+        (item.aptNotes.toLowerCase().indexOf(queryText) != -1) 
+        ) {
+        filteredApts.push(item);
+      }
+    });
+
+    filteredApts = _.orderBy(filteredApts, function(item){
+      return item[orderBy].toLowerCase();
+    }, orderDir);
 
     filteredApts = filteredApts.map(function(item, index){
       return (
@@ -68,6 +105,13 @@ var MainInterface = React.createClass({
           handleToggle = { this.toggleAddDisplay }
           addApt = { this.addItem }
         />
+
+        <SearchAppointments 
+        orderBy = { this.state.orderBy }
+        orderDir = { this.state.orderDir } 
+        onReOrder = {this.reOrder }
+        onSearch = {this.searchApts}/>
+
         <ul className="item-list media-list">
           {filteredApts}
         </ul>

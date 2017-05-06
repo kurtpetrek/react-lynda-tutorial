924fcf4b9788429e1044f3943dda2f8d55c05f0f
Kurt Petrek <kurtpetrek@gmail.com>
2017-05-06 17:47:06 -0400

    added delete functionality

--- process/js/app.js
+++ process/js/app.js
@@ -1,5 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var _ = require('lodash');
+
+var AptList = require('./AptList')
 
 var MainInterface = React.createClass({
   getInitialState: function(){
@@ -15,32 +18,29 @@ var MainInterface = React.createClass({
         myAppointments: tempApts
       }); //setState
     }.bind(this));
-  },
+  }, //componentDidMount
 
   componentWillUnmount: function(){
     this.serverRequest.abort();
-  },
+  }, //componentWillUnmount
+
+  deletMessage: function(item){
+    var allApts = this.state.myAppointments;
+    var newApts = _.without(allApts, item);
+    this.setState({
+      myAppointments: newApts
+    }); //setState
+  }, //deleteMessage
 
   render: function() {
     var filteredApts = this.state.myAppointments;
 
     filteredApts = filteredApts.map(function(item, index){
       return (
-          <li className="pet-item media" key={index}>          
-            <div className="pet-info media-body">
-              <div className="pet-head">
-                <span className="pet-name">{ this.state.myAppointments[index].petName }</span>
-                <span className="apt-data pull-right">{ this.state.myAppointments[index].aptDate} </span>
-              </div>
-              <div className="owner-name">
-                <span className="label-item">Owner: </span>
-                {this.state.myAppointments[index].ownerName}
-              </div>
-              <div className="apt-notes">
-                {this.state.myAppointments[index].aptNotes}
-              </div>
-            </div>
-          </li>
+        <AptList key = {index}
+            singleItem = {item}
+            whichItem = {item}
+            onDelete = {this.deletMessage} />
       ) // return
     }.bind(this)); // filteredApts.map
 
@@ -53,6 +53,7 @@ var MainInterface = React.createClass({
     ) //return
   } //render
 }); //MainInterface
+
 
 ReactDOM.render(
   <MainInterface />,
